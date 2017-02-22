6522c584dda482fb6b1ae4456d47cb5e5859eca9
masafumi kato <xslash1105@gmail.com>
2017-02-22 22:38:32 +0900

    changed db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/login', to: 'users#new'
+  post '/login', to: 'users#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
