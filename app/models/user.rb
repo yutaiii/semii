2c631d1163a7d05169a5c7f0fc995654f2a4a017
masafumi kato <xslash1105@gmail.com>
2017-04-24 20:32:58 +0900

    新規登録のエラーメッセージを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ActiveRecord::Base
   validates :department_id, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+def email_required?
+  false
+end
+def password_required?
+  false
+end
 end
