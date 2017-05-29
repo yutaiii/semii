f18c5f6c410a0a0d81204c0b58f17b50d0d26e37
市川裕太 <yu74ichi69@gmail.com>
2017-05-29 11:53:45 +0900

    ログイン時のヘッダー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,15 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  #deviseに含まれていないカラムを許可
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:faculty_id])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:department_id])
+  end
+
+  #ログアウトした後のリダイレクト先
+  def after_sign_out_path_for(resource)
+    new_user_session_path
   end
 
 end
