7e659334a68d486363f87995e47f8fb29d440e92
市川裕太 <yu74ichi69@gmail.com>
2017-05-29 17:48:10 +0900

    seed.rbに学部を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# coding: utf-8
+
+#facultyテーブルに学部を入れる
+
+  Faculty.create(name: '人文学部')
+  Faculty.create(name: '外国語学部')
+  Faculty.create(name: '経済学部')
+  Faculty.create(name: '経営学部')
+  Faculty.create(name: '法学部')
+  Faculty.create(name: '総合政策学部')
+  Faculty.create(name: '理工学部')
+  Faculty.create(name: '国際教養学部')
+  Faculty.create(name: '短期大学部')
