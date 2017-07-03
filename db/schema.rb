5ef5d0ab1ddb7f8abf18a9e298849136293fda3b
masafumi kato <xslash1105@gmail.com>
2017-07-03 19:21:25 +0900

    サインインしたらマイページに行けるよう修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170614051534) do
 
   create_table "departments", force: :cascade do |t|
